0bdb499afa4233da77d79eaff925bdbbe305ade0
GeorgeR123 <151932059+GeorgeR123@users.noreply.github.com>
2024-01-14 19:03:16 +0200

    Meniul START / LEVEL SELECTOR

---
+++ quizGame/LevelMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quizGame
+{
+    public partial class LevelMenu : Form
+    {
+        public LevelMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            QLevel1 gameWindow = new QLevel1();
+
+            gameWindow.Show();
+        }
+    }
+}
---
+++ quizGame/StartMenu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace quizGame
+{
+    public partial class StartMenu : Form
+    {
+        public StartMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadLevelMenu(object sender, EventArgs e)
+        {
+            LevelMenu gameWindow = new LevelMenu();
+
+            gameWindow.Show();
+        }
+    }
+}
